c1d538a475a5d069496e37cebdc8dbf9fd14cf50
Kyle Zamora <58868153+kjzamora@users.noreply.github.com>
2022-01-29 09:46:58 -0500

    first commit

---
+++ EventViewer_System_ErrorLog.ps1
@@ -0,0 +1,24 @@
+﻿$fileDate = Get-Date -UFormat "%Y_%m_%d"
+$outFilePath = "C:\logs\event_viewer\"
+$fileName = "System_Errors_$($fileDate).txt"
+$fileNameCsv = "System_Errors.csv"
+$fullPath = $outFilePath + $fileName
+$fullPathCsv = $outFilePath + $fileNameCsv
+
+# Check for folder path and create it
+If (Test-Path -Path $outFilePath)
+{
+    # Continue if path exist
+}
+else
+{
+    New-Item -Path $outFilePath -ItemType Directory
+}
+
+# Export csv and txt logs
+$sysErrors = Get-EventLog -LogName System -EntryType Error
+$sysErrors | Export-Csv $fullPathCsv -NoTypeInformation
+foreach ($err in $sysErrors)
+{
+    $err | Format-Table -Wrap -AutoSize | Out-File -FilePath $fullPath -Append
+}
